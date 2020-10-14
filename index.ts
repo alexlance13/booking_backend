5ab0a05d3c471474787d52e4c0db94a99051fd3b
Alex <Alexlance13@gmail.com>
2020-10-14 11:42:05 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+// Construct a schema, using GraphQL schema language
+const typeDefs = require('./typeDefs.ts');
+// Provide resolver functions for your schema fields
+
+const resolvers = require('./resolvers.ts');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
